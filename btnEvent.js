98f2494d92780e4977fce95844d5575d609d83ba
salaschen <ruowei.chen.aus@gmail.com>
2021-07-09 21:02:16 +1000

    made UI changes, now calculate and additional deposit are different buttons.

--- btnEvent.js
+++ btnEvent.js
@@ -1,86 +1,71 @@
+// global variable for additional investment
+var cur_id = 1;
+var deposit_limit = 2 ; // number of additional investment can be made. Subject to change.
 
-var id = 1;
-
-// Create node 
+// Create node
 const addElement = (type) => {
-    let name = document.createElement(type);
-    return name;
-}
+    let element = document.createElement(type);
+    return element;
+};
+
 // Create <label> node with for attribute
 const addLabel = (txt, i) => {
-    
-    let forId = "input" + id + i;
+    let forId = "input" + cur_id + i;
     let label = addElement("LABEL");
     let t = document.createTextNode(txt);
     label.setAttribute("for", forId);
     label.appendChild(t);
     return label;
-}
+};
+
 // Create <label> node with attributes
-const addInput = (i) => {
-    let inputId = "input" + id + i;
+const addInput = (i, defaultInputValue=null) => {
+    let inputId = "input" + cur_id + i;
     let input = addElement("INPUT");
     input.setAttribute("id", inputId);
     input.setAttribute("inputmode", "numeric");
     input.setAttribute("type", "text");
     input.setAttribute("aria-required", "true");
+    input.setAttribute("value",  defaultInputValue) ; 
     return input;
-}
+};
+
 // Create the node pair of <label> and <input>
-const addPair = (txt, i) => {
-    
-    let div = addElement("DIV");
+const addPair = (txt, i, defaultInputValue) => {
+    let div = addElement("p");
     let label = addLabel(txt, i);
-    let input = addInput(i);
+    let input = addInput(i, defaultInputValue);
     div.appendChild(label);
     div.appendChild(input);
-
     return div;
-}
+};
+
 // Create the node block of 2 pairs with className
 const addBlock = () => {
-    let block = addElement("DIV");   
-    let pair1 = addPair("additional contribution", "a");
-    let pair2 = addPair("year", "b");
-    block.appendChild(pair1);
-    block.appendChild(pair2);
-    block.classList.add("flex");
+    let pair1 = addPair("Additional investment " + cur_id + ":", "a", 10000);
+    let pair2 = addPair("At Year:", "b", 5);
 
-    return block;
-}
+    // place holder, hidden
+    let pair3 = addPair("", "", "") ;
+    pair3.children[1].setAttribute("style", "background-color:#fbfbfb;border:0px") ; 
+    return [pair1, pair2, pair3];
+};
+
 // Create the documentFragment
 const addContainer = () => {
-    let form = document.getElementById("form");
-    let div1 = addElement("DIV");
-    let div2 = addElement("DIV");
-    let div3 = addBlock();    
-    let header = addElement("H3");
-    let t = document.createTextNode("Additional Contribution Strategy");
-
-    header.classList.add("col100");
-    header.appendChild(t);
-    div2.classList.add("additionalStrategy");
-    div2.appendChild(header);
-    div2.appendChild(div3);
-    div1.appendChild(div2);
-    form.appendChild(div1);
-}
+    let addButton = document.getElementById("additionalDepositButton") ;
+    let container = document.getElementsByClassName("yourStrategy")[0] ;
+    addBlock().forEach(pair =>  
+        container.insertBefore(pair, addButton) 
+    );
+    cur_id += 1;
+    if (cur_id > deposit_limit) {
+        addButton.setAttribute("style", "visibility:hidden") ;
+    }
+};
 
 const confirmBtn = () => {
-    if (document.getElementById("form").childElementCount < 2)// validate whether the time of additional investments reachs 2
-     {
+    if (cur_id <= 2) {
         addContainer();
-        id ++;
-    } else {
-        chart();
-    }
-}
-
-
-
-
-
-
-
-
-
+    } 
+};
